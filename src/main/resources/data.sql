1a80e4071fee5cced9c15e0736ca761a9bcaf03d
Adriano-Ivan <adrianoivandepaulacampos@gmail.com.br>
2022-02-19 18:27:09 -0300

    Adição de imagem Docker e deploy no Heroku.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO usuarios(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+INSERT INTO usuarios(nome, email, senha) VALUES('Moderador', 'moderador@email.com', '$2a$10$sFKmbxbG4ryhwPNx/l3pgOJSt.fW1z6YcUnuE2X8APA/Z3NI/oSpq');
+
+INSERT INTO PERFIS(id, nome) VALUES(1, 'ROLE_ALUNO');
+INSERT INTO PERFIS(id, nome) VALUES(2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIOS_PERFIS(usuario_id, perfis_id) VALUES(1, 1);
+INSERT INTO USUARIOS_PERFIS(usuario_id, perfis_id) VALUES(2, 2);
+
+INSERT INTO CURSOS(nome, categoria) VALUES('Spring Boot', 'Programação');
+INSERT INTO CURSOS(nome, categoria) VALUES('HTML 5', 'Front-end');
+
+INSERT INTO TOPICOS(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICOS(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICOS(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
--- src/test/java/br/com/adriano/forum/repository/CursoRepositoryTest.java
+++ src/test/java/br/com/adriano/forum/repository/CursoRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
 import br.com.adriano.forum.modelo.Curso;
@@ -14,38 +15,30 @@ import br.com.adriano.forum.modelo.Curso;
 @AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
 class CursoRepositoryTest {
-	
 	@Autowired
 	private CursoRepository repository;
 	
-	/*
-	 @Autowired
-	 private TestEntityManager em;
-	 */
-	
+	@Autowired
+	private TestEntityManager em;
+
 	@Test
 	public void deveriaCarregarUmCursoAoBuscarPeloSeuNome() {
-		String nomeCurso = "HTML 5"; 
+		String nomeCurso = "HTML 5";
 		
-		/*
-		 Curso curso = new Curso();
-		 curso.setNome("HTML 5");
-		 curso.setCategoria("Front-end");
-		 em.persist(curso);
-		 */
+		Curso html5 = new Curso();
+		html5.setNome(nomeCurso);
+		html5.setCategoria("Programacao");
+		em.persist(html5);
 		
 		Curso curso = repository.findByNome(nomeCurso);
-		
 		assertNotNull(curso);
 		assertEquals(nomeCurso, curso.getNome());
 	}
-
+	
 	@Test
-	public void naoDeveriaCarregarUmCursoAoBuscarPeloSeuNome() {
-		String nomeCurso = "JPA"; 
-		
+	public void naoDeveriaCarregarUmCursoCujoNomeNaoEstejaCadastrado() {
+		String nomeCurso = "JPA";
 		Curso curso = repository.findByNome(nomeCurso);
-		
 		assertNull(curso);
 	}
 }
